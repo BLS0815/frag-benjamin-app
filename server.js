214164facdf76dcd7e3f72eefef76a650404f409
BLS0815 <b.lucas.s@web.de>
2026-04-18 10:48:48 +0200

    express.static auf __dirname gefixt für Railway

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
 const client = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
